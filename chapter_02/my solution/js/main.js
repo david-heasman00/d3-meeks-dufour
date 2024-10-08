4cc355fa636825e4de1cc406705d3429afc32552
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2024-10-08 10:16:15 +0100

    upload of chapter 2 exercise files

---
+++ chapter_02/my solution/js/main.js
@@ -0,0 +1,7 @@
+//Append a responsive svg to div
+const svg = d3.select(".responsive-svg-container")
+  .append("svg")
+    .attr("viewBox", "0 0 1200 1600")
+    .style("border", "1px solid black");
+
+
