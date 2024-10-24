05a598b381535a17d202bb5ce7e33d6907fd7c95
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2024-10-24 10:50:08 +0100

    Update arc.js in exercise 4.4

--- chapter_04/4.4-Drawing_arcs/my solution/js/arcs.js
+++ chapter_04/4.4-Drawing_arcs/my solution/js/arcs.js
@@ -1,7 +1,29 @@
 // Load the data here
-
+d3.csv("data/daily_precipitations.csv", d3.autoType).then(data => {
+    console.log("precipitation data", data);
+    drawArc(data);
+});
 
 // Draw the arc here
 const drawArc = (data) => {
+  //Set height and width constants of the arc
+  const pieChartWidth = 300;
+  const pieChartHeight = 300;
+  
+  //Append responsive SVG container
+  const svg = d3.select("#arc")
+    .append("svg")
+    .attr("viewBox", [0, 0, pieChartWidth, pieChartHeight]);
 
+  //Create Arc Container Group in SVG container
+  const innerChart = svg
+    .append("g")
+      .attr("transform", `translate(${pieChartWidth/2}, ${pieChartHeight/2})`);  //translate arc chart to centre of svg container
+
+  //Calculate variables for pie chart (can use Arc Generator, but doing it here quickly)
+  const numberOfDays = data.length;
+  const numberOfDaysWithPrecipitation = data.filter(d => d.total_precip_in >0).length;
+  const percentageDaysWithPrecipitation = Math.round(numberOfDaysWithPrecipitation / numberOfDays * 100);
+  const angleDaysWithPrecipitation_deg = percentageDaysWithPrecipitation * 360 / 100;
+  const angleDaysWithPrecipitation_rad = angleDaysWithPrecipitation_deg * Math.PI / 180;
 };
