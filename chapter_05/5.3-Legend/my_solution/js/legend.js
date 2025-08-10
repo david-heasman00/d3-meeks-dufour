a08d7597ec5f0bf2aeafe8e8ca0a2a34756f8df0
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2025-08-10 14:26:30 +0100

    Added legend

--- chapter_05/5.3-Legend/my_solution/js/legend.js
+++ chapter_05/5.3-Legend/my_solution/js/legend.js
@@ -1,5 +1,27 @@
+/*********************/
+/* Create the Legend */
+/*********************/
 const addLegend = () => {
-  // Create the legend here
 
+  //Create list of legend items
+  const legendItems = d3.select(".legend-container")
+    .append("ul")
+      .attr("class", "color-legend")
+    .selectAll(".color-legend-item")
+    .data(formatsInfo)
+    .join("li")
+      .attr("class", "color-legend-item");
+
+  //Span each list item and set background colour
+  legendItems
+    .append("span")
+      .attr("class", "color-legend-item-color")
+      .style("background-color", d => d.color);
+
+  //Set text to correspond to label of music format
+  legendItems
+    .append("span")
+      .attr("class", "color-legend-item-label")
+      .text(d => d.label);
   
 };
