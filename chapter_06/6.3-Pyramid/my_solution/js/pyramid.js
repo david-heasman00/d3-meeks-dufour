04fe389058bc26a4143015dcbea7974ae5ad55ed
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2025-08-19 08:37:06 +0100

    Creating pyramid chart
    Unfinished

--- chapter_06/6.3-Pyramid/my_solution/js/pyramid.js
+++ chapter_06/6.3-Pyramid/my_solution/js/pyramid.js
@@ -23,5 +23,58 @@ const drawPyramid = (data) => {
     .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+  /******************************/
+  /*    Generate bins           */
+  /******************************/
+  const binGenerator = d3.bin()
+    .value(d => d.salary);
   
+  //Male Bin Generator
+  const maleBins = binGenerator(data.filter(d => d.gender === "Male"));
+
+  //Female Bin Generator
+  const femaleBins = binGenerator(data.filter(d => d.gender === "Female"));
+
+  /****************************/
+  /*    Declare the scales    */
+  /****************************/
+
+  //X Male Scale
+  const xMaleScale = d3.scaleLinear()
+    .domain([0, 15])
+    .range([innerWidth/2, innerWidth]);
+
+  //X Female Scale
+  const xFemaleScale = d3.scaleLinear()
+    .domain([15,0])
+    .range([0, innerWidth/2]);
+
+  //Y Scale
+  const minSalary = femaleBins[0].x0;
+  const maxSalary = femaleBins[femaleBins.length -1].x1;
+  const yScale = d3.scaleLinear()
+    .domain([minSalary, maxSalary])
+    .range(innerHeight, 0)
+    .nice();
+
+  /*******************************/
+  /*    Append the pyramids      */
+  /*******************************/
+  const pyramidContainer = innerChart
+    .append("g")
+      .attr("stroke", white)
+      .attr("stroke-width", 2);
+  
+  //Female Pyramids
+  pyramidContainer
+    .selectAll(".bar-women")
+    .data(femaleBins)
+    .join("rect")
+      .attr("class", "bar-women")
+      .attr("x", d => xFemaleScale(d.length / data.length * 100))
+      .attr("y", d => yScale(d.x1))
+      .attr("width", d => innerWidth/2 - xFemaleScale(d.length / data.length * 100))
+      .attr("height", d => yScale(d.x0) - yScale(d.x1))
+      .attr("fill", womenColor);
+
 };
