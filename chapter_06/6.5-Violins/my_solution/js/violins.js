0430c2d1cf492c66ec6b48fe84407dda0280fcb9
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2025-08-22 08:41:38 +0100

    Update violins.js
    Calculate quartiles and other variables for violinplot.

--- chapter_06/6.5-Violins/my_solution/js/violins.js
+++ chapter_06/6.5-Violins/my_solution/js/violins.js
@@ -27,6 +27,7 @@ const drawViolinCharts = (data) => {
   /*********************************/
   /*    Calculate the quartiles    */
   /*********************************/
+  //Create roles array
   const roles = [
     {id: "Designer" },
     {id: "Scientist" },
@@ -35,5 +36,58 @@ const drawViolinCharts = (data) => {
     {id: "Leadership" },
   ];
   
+  //Loop through roles array and perform a bunch of calculations
+  roles.forEach(role => {
+
+    //Map salaries for current role into roles array with "salaries" key
+    role["salaries"] = data
+      .filter(d -> d.role === role.id)
+      .map(d => d.salary);
+
+    //Calc mean value and store in "means" key in roles array
+    role["mean"] = d3.mean(role.salaries);
+
+    //Put salaries into bins under "bins" key in roles array
+    role["bins"] = d3.bin()(role.salaries);
+
+    //Calculate quartiles and put into "quartiles" key in roles array
+    const quartilesScale = d3.scaleQuantile()
+      .domain(role.salaries)
+      .range([0, 1, 2, 3]);
+    role["quartiles"] = quartilesScale.quantiles();
+  });
+
+  //Sort roles array in ascending order of mean value
+  roles.sort((a, b) => a.mean - b.mean);
+
+  /*********************************/
+  /*    Declare scales             */
+  /*********************************/
+  //x point Scale to spread roles along x axis
+  const xScale = d3.scalePoint()
+    .domain(roles.map(d => d.id))
+    .range([0, innerWidth])
+    .padding(0.7);
+
+  //Linear scale to distribute salaries on y axis  
+  const maxSalary = d3.max(data, d => d.salary);
+  const yScale = d3.scaleLinear()
+    .domain([0, maxSalary])
+    .range([innerHeight, 0])
+    .nice();
+
+  //Find length of tallest bin for the roles
+  let maxBinLength = 0;
+  roles.forEach(role => {
+    const max = d3.max(role.bins, d => d.length);
+    if (max > maxBinLength) {
+      maxBinLength = max;
+    }
+  })
+
+  //Declare linear scale responsible for calculating width of violin plot for each salary bracket
+  const violinsScale = d3.scaleLinear()
+    .domain([0, maxBinLength])
+    .range([0, xScale.step()/2]);
 
 };
