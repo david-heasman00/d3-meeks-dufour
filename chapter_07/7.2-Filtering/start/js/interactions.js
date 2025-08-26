ecb0303855183258fe40b2b90338fb70c68fee90
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2025-08-26 15:56:56 +0100

    Update load.js & interaction.js
    Change data file path, and experiment with event listeners after adding filter buttons to histogram.

--- chapter_07/7.2-Filtering/start/js/interactions.js
+++ chapter_07/7.2-Filtering/start/js/interactions.js
@@ -3,8 +3,20 @@
 /*************************************/
 const populateFilters = (data) => {
 
-  // Create the filters here
-
+  d3.select("#filters")                               //Select div #filters
+    .selectAll(".filter")                             //Data binding pattern adding
+    .data(filters)                                    //button for each obj in filter array
+    .join("button")
+      .attr("class", d => `filter ${d.isActive        //Set button class to name of 'filter'
+        ? "active"                                    //if isActive = true, add class name "active"
+        : ""                                          //Else no class
+      }`)
+      .text(d => d.label)                             //Set button text using filter's label property 
+      //Add event lister
+      .on("click", (e, d) => {
+        console.log("DOM event", e)
+        console.log("Attached datum", d);
+      });
 };
 
 
