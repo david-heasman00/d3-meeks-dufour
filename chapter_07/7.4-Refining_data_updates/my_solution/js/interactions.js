1c0fe3d41f3c4ba03fc79596e4e8f99878595334
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2025-08-29 16:10:01 +0100

    Update interactions.js
    Use enter, update, and exit selections to add smoother transitions

--- chapter_07/7.4-Refining_data_updates/my_solution/js/interactions.js
+++ chapter_07/7.4-Refining_data_updates/my_solution/js/interactions.js
@@ -47,20 +47,46 @@ const handleClickOnFilter = (data) => {
           ? data
           : data.filter(d => d.hemisphere === datum.id);
 
+        //Define transition constant
+        const transitionConstant = d3.transition()
+          .duration(800)
+          .ease(d3.easeExpOut);
+        
         //Data binding pattern to update no of circles on screen, set position, radius and color
         innerChart
           .selectAll("circle")
-          .data(updatedData)
-          .join("circle")
-          .transition()
-            .attr("class", "cetacean")
-            .attr("cx", d => xScale(d.global_population_estimate))
-            .attr("cy", d => yScale(d.max_size_m))
-            .attr("r", d => ReadableByteStreamController(d.max_weight_t))
-            .attr("fill", d => colorScale(d.status))
-            .attr("fill-opacity", 0.6)
-            .attr("stroke", d => colorScale(d.status))
-            .attr("stroke-width", 2);
+          .data(updatedData, d => d.uid)                //Add key reference for enter, exit selection
+          .join(
+            //Enter selection
+            enter => enter
+              .append("circle")
+                .attr("class", "cetacean")
+                .attr("cx", d => xScale(d.global_population_estimate))
+                .attr("cy", d => -50)                                       //Set circles to start off screen
+                .attr("r", 0)                                               //Set circles to be 0 size
+                .attr("fill", d => colorScale(d.status))
+                .attr("fill-opacity", 0.6)
+                .attr("stroke", d => colorScale(d.status))
+                .attr("stroke-width", 2)
+                .attr("opacity", 0)                                         //Set circles to be invisible
+              //Call method, transitioning enter selection to where we want them to go
+              .call(enter => enter.transition(transitionConstant)                             
+                .attr("cy", d => yScale(d.max_size_m))
+                .attr("r", d => rScale(d.max_weight_t))
+                .style("opacity", 1)
+              ),
+            //Update selection. Still chained even though we don't want anything to happen
+            update => update,
+            
+            //Exit selection
+            exit => exit
+              .call(exit => exit.transition(transitionConstant)
+                .attr("cy", d => innerHeight)                                 //Set circles to exit off screen
+                .attr("r", 0)                                                 //And to have zero size
+                .style("opacity", 0)                                          //And to be invisible
+                .remove()                                                     //And to be removed from the DOM
+            )
+          )
       }
     });
 
