f28a3b6beff4521e66f16a9071dcf338688dfadd
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2025-08-29 15:48:15 +0100

    Update interactions.js
    Scatterplot changes based upon filter press now

--- chapter_07/7.4-Refining_data_updates/my_solution/js/interactions.js
+++ chapter_07/7.4-Refining_data_updates/my_solution/js/interactions.js
@@ -20,6 +20,48 @@ const populateFilters = () => {
 
 const handleClickOnFilter = (data) => {
 
-  // Handle clicks on filters here
+  //Select all buttons with class filter
+  d3.selectAll(".filter")
+
+    //Attach event listener for "click" event
+    .on("click", (e, datum) => {
+
+      //Check clicked button isn't already selected
+      if (!datum.isActive) {
+
+        //Update isActive properties for cetaceanFilters array
+        cetaceanFilters.forEach(h => h.isActive = h.id === datum.id
+          ? true
+          : false
+        );
+
+        //Update "active" class name appropriately to above
+        d3.selectAll(".filter")
+          .classed("active", d => d.id === datum.id
+            ? true
+            : false
+          );
+
+        //Filter original dataset to conserve only cetaceans corresponding to selection  
+        const updatedData = datum.id === "all"
+          ? data
+          : data.filter(d => d.hemisphere === datum.id);
+
+        //Data binding pattern to update no of circles on screen, set position, radius and color
+        innerChart
+          .selectAll("circle")
+          .data(updatedData)
+          .join("circle")
+          .transition()
+            .attr("class", "cetacean")
+            .attr("cx", d => xScale(d.global_population_estimate))
+            .attr("cy", d => yScale(d.max_size_m))
+            .attr("r", d => ReadableByteStreamController(d.max_weight_t))
+            .attr("fill", d => colorScale(d.status))
+            .attr("fill-opacity", 0.6)
+            .attr("stroke", d => colorScale(d.status))
+            .attr("stroke-width", 2);
+      }
+    });
 
 };
